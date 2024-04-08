6d1db72aeb6c38dafea3be80856784819a4a401a
gblow <144559335+gblow@users.noreply.github.com>
2024-04-07 18:10:02 -0700

    Updated and removed duplicate code

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable no-unused-vars */
-// see SignupForm.js for comments
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
@@ -15,6 +13,14 @@ const LoginForm = () => {
 
   const [login, { error }] = useMutation(LOGIN_USER);
 
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -23,7 +29,6 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -35,17 +40,14 @@ const LoginForm = () => {
         variables: { ...userFormData },
       });
 
-      
-
-       
+      console.log(data);
       Auth.login(data.login.token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+    } catch (e) {
+      console.error(e);
     }
 
+    // clear form values
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });

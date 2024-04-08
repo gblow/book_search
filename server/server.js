429d864577d4a8a7c73ef4e3dd8f5dae905d02dd
gblow <144559335+gblow@users.noreply.github.com>
2024-04-08 09:43:19 -0700

    fixed typo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { expressMiddleware } = require('@apollo/server/express4');
+const { expressMiddleware } = require('@apollo/server/express');
 const { ApolloServer } = require('@apollo/server');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
